f925f37b82531f9f8262cd1332ba99fd77bd4de8
Daniel Dang <89473635+dqduong2003@users.noreply.github.com>
2025-09-10 15:33:36 +1000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -50,6 +50,7 @@ function App() {
               <span className="tech-tag">Express</span>
               <span className="tech-tag">GitHub</span>
               <span className="tech-tag">Jenkins</span>
+              <span className="tech-tag">CursorAI</span>
             </div>
           </div>
           
