01cb78699e45bdc638687b90347e9e00517873e3
Daniel Dang <89473635+dqduong2003@users.noreply.github.com>
2025-09-11 13:06:18 +1000

    Delete Jenkins

--- client/src/App.js
+++ client/src/App.js
@@ -49,7 +49,6 @@ function App() {
               <span className="tech-tag">Node.js</span>
               <span className="tech-tag">Express</span>
               <span className="tech-tag">GitHub</span>
-              <span className="tech-tag">Jenkins</span>
               <span className="tech-tag">CursorAI</span>
             </div>
           </div>
